9f30c62c1a67725ab8dcd3fe0ad9d9f800126bf9
fernandogatto <fernandogatto17@gmail.com>
2024-03-28 18:09:43 -0300

    chore: add config api client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { ThemeProvider } from './components/theme/theme-provider'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 
-import { router } from './routes'
 import './global.css'
+import { queryClient } from './lib/react-query'
+import { router } from './routes'
 
 export function App() {
   return (
@@ -13,7 +15,10 @@ export function App() {
         <Helmet titleTemplate="%s | Pizza Shop" />
 
         <Toaster richColors />
-        <RouterProvider router={router} />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
