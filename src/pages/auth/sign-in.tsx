b86df368f1cf3aac54c8fcef46c410adfd90ff22
fernandogatto <fernandogatto17@gmail.com>
2024-04-02 20:25:05 -0300

    feat: add user authentication with react query

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
@@ -5,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import { toast } from 'sonner'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -24,9 +26,13 @@ export function SignIn() {
     resolver: zodResolver(schema),
   })
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSubmitSignIn(data: SignInFormInputs) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
@@ -43,34 +49,34 @@ export function SignIn() {
 
   return (
     <>
-      <Helmet title="Login" />
+      <Helmet title='Login' />
 
-      <div className="p-8">
-        <Button asChild variant="outline" className="absolute right-8 top-8">
-          <Link to="/sign-up">Criar novo estabelecimento</Link>
+      <div className='p-8'>
+        <Button asChild variant='outline' className='absolute right-8 top-8'>
+          <Link to='/sign-up'>Criar novo estabelecimento</Link>
         </Button>
 
-        <div className="flex w-[350px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
+        <div className='flex w-[350px] flex-col justify-center gap-6'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>
               Acessar painel
             </h1>
-            <p className="text-sm text-muted-foreground">
+            <p className='text-sm text-muted-foreground'>
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
 
           <form
             onSubmit={handleSubmit(handleSubmitSignIn)}
-            className="space-y-4"
+            className='space-y-4'
           >
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input {...register('email')} type="email" />
+            <div className='space-y-2'>
+              <Label htmlFor='email'>Seu e-mail</Label>
+              <Input {...register('email')} type='email' />
               {errors.email && <span>{errors.email.message}</span>}
             </div>
 
-            <Button disabled={isSubmitting} className="w-full" type="submit">
+            <Button disabled={isSubmitting} className='w-full' type='submit'>
               Acessar painel
             </Button>
           </form>
