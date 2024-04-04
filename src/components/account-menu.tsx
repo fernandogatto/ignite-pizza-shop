116d3ee3444762dbdeb65ce5774e9197774e4d14
fernandogatto <fernandogatto17@gmail.com>
2024-04-04 19:53:40 -0300

    feat: add connection with api to profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+
 import { Button } from './ui/button'
 import {
   DropdownMenu,
@@ -9,33 +11,46 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 
+import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from '@/api/get-managed-restaurant'
+
 export function AccountMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button
-          variant="outline"
-          className="flex items-center gap-2 select-none"
+          variant='outline'
+          className='flex items-center gap-2 select-none'
         >
-          Pizza Shop
-          <ChevronDown className="h-4 w-4" />
+          {managedRestaurant?.name}
+          <ChevronDown className='h-4 w-4' />
         </Button>
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
-        <DropdownMenuLabel className="flex flex-col">
-          <span>Fernando</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            fernandogatto17@gmail.com
+      <DropdownMenuContent align='end' className='w-56'>
+        <DropdownMenuLabel className='flex flex-col'>
+          <span>{profile?.name}</span>
+          <span className='text-xs font-normal text-muted-foreground'>
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          <Building className="h-4 w-4 mr-2" />
+          <Building className='h-4 w-4 mr-2' />
           <span>Perfil da loja</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="h-4 w-4 mr-2" />
+        <DropdownMenuItem className='text-rose-500 dark:text-rose-400'>
+          <LogOut className='h-4 w-4 mr-2' />
           <span>Sair</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
