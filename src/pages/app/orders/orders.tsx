a3145b721c23292ed75d8ad24ae280023de3d3a0
fernandogatto <fernandogatto17@gmail.com>
2024-04-17 19:05:03 -0300

    feat: add orders list

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -10,37 +10,46 @@ import {
 import { Pagination } from '@/components/pagination'
 
 import { OrderTableFilters } from './order-table-filters'
-import { OrderTableRow } from './order-table-row'
+import { OrderTableRow, OrderTableRowProps } from './order-table-row'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '@/api/get-orders'
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders,
+  })
+
   return (
     <>
-      <Helmet title="Pedidos" />
+      <Helmet title='Pedidos' />
 
-      <div className="flex flex-col gap-4">
-        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-3xl font-bold tracking-tight'>Pedidos</h1>
 
-        <div className="space-y-2.5">
+        <div className='space-y-2.5'>
           <OrderTableFilters />
 
-          <div className="rounded-md border">
+          <div className='rounded-md border'>
             <Table>
               <TableHeader>
                 <TableRow>
-                  <TableHead className="w-[64px]"></TableHead>
-                  <TableHead className="w-[140px]">Identificador</TableHead>
-                  <TableHead className="w-[180px]">Realizado há</TableHead>
-                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead className='w-[64px]'></TableHead>
+                  <TableHead className='w-[140px]'>Identificador</TableHead>
+                  <TableHead className='w-[180px]'>Realizado há</TableHead>
+                  <TableHead className='w-[140px]'>Status</TableHead>
                   <TableHead>Cliente</TableHead>
-                  <TableHead className="w-[140px]">Total do pedido</TableHead>
-                  <TableHead className="w-[164px]"></TableHead>
-                  <TableHead className="w-[132px]"></TableHead>
+                  <TableHead className='w-[140px]'>Total do pedido</TableHead>
+                  <TableHead className='w-[164px]'></TableHead>
+                  <TableHead className='w-[132px]'></TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return <OrderTableRow key={i} />
-                })}
+                {result &&
+                  result.orders &&
+                  result.orders.map(order => (
+                    <OrderTableRow key={order.orderId} order={order} />
+                  ))}
               </TableBody>
             </Table>
           </div>
